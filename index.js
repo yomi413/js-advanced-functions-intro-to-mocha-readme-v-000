6eff34b82cd1c48ff49266b1d5b9617cde630f03
Yomaira Escano <yjescano@gmail.com>
2018-10-12 16:18:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  const greeting = 'Hey!'
+  return greeting
+}
+
+function sayHeyFriend(name) {
+  return  `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(statement) {
+  return statement.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
